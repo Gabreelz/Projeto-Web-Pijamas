a44fdf35b91cbb2cdc5c516ecfdedf3dd5e6090c
Felip4n <arantes.felip25@gmail.com>
2025-06-09 21:49:27 -0300

    Correção catalogo

--- Catalogo/Script.js
+++ Catalogo/Script.js
@@ -1,6 +1,6 @@
 function scrollCarrossel(id, direction) {
   const carrossel = document.getElementById(id);
-  const cardWidth = 260; // Largura aproximada de cada produto + gap
+  const cardWidth = 260; 
   carrossel.scrollBy({
     left: direction * cardWidth * 2,
     behavior: 'smooth'
